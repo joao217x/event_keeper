1a787b0cf083d3399f4150d65abb5cb70dbc8dd2
João Marchizio <jpmarchizio@gmail.com>
2022-11-21 18:32:56 -0300

    firebase insert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:event_keeper/event_keeper.dart';
+import 'package:event_keeper/firebase_options.dart';
 import 'package:event_keeper/shared/util/app_dependencies.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const EventKeeper());
 }
