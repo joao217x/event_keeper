3dc58e36f70b7bc190db9aad11974fbe850a1752
João Marchizio <jprm_11@hotmail.com>
2022-11-21 18:27:32 -0300

    instances get it

--- lib/shared/util/app_dependencies.dart
+++ lib/shared/util/app_dependencies.dart
@@ -1,7 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:event_keeper/features/login/service/firebase_service.dart';
+import 'package:event_keeper/features/login/service/interface/firebase_service_interface.dart';
+import 'package:event_keeper/shared/client/api/dio_impl.dart';
+import 'package:event_keeper/shared/client/api/interface/api_client_interface.dart';
+import 'package:event_keeper/shared/client/firebase/firebase_impl.dart';
+import 'package:event_keeper/shared/client/firebase/interface/firebase_client_interface.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance();
 
 class AppDependencies {
-  void setup() {}
+  void setup() {
+    //Libs
+    getIt.registerFactory<Dio>(() => Dio());
+    getIt.registerFactory<FirebaseAuth>(() => FirebaseAuth.instance);
+
+    //Implementations
+    getIt.registerFactory<ApiClientInterface>(() => DioImpl());
+    getIt.registerFactory<FirebaseClientInterface>(() => FirebaseImpl());
+
+    //Services
+    getIt.registerFactory<FirebaseServiceInterface>(
+      () => FirebaseService(firebaseClient: getIt<FirebaseClientInterface>()),
+    );
+  }
 }
