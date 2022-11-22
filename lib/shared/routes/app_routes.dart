f4c273feaa255ec143221d8ddf76f5f2285448d8
João Marchizio <jpmarchizio@gmail.com>
2022-11-22 18:49:37 -0300

    rotas add

--- lib/shared/routes/app_routes.dart
+++ lib/shared/routes/app_routes.dart
@@ -1,16 +1,12 @@
+import 'package:event_keeper/features/login/view/login_screen.dart';
+import 'package:event_keeper/features/splash/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes(context) {
     return {
-      // '/splash': (context) => const SplashScreen(),
-      // '/login': (context) => const LoginScreen(),
-      // '/forget': (context) => const ForgetPasswordScreen(),
-      // '/newEvent': (context) => const NewEventScreen(),
-      // '/tabs': (context) => const TabsScreen(),
-      // '/softEventInfo': (context) => const SoftEventInfoTab(),
-      // '/softEventPlace': (context) => const SoftEventPlaceTab(),
-      // '/myEventInfo': (context) => const MyEventInfoTab(),
+      '/splash': (context) => const SplashScreen(),
+      '/login': (context) => LoginScreen(),
     };
   }
 }
