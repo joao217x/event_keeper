7f3d8251f43812ce5a4a0aca0ab3f1778ee9e3bc
João Marchizio <jpmarchizio@gmail.com>
2022-11-25 12:08:34 -0300

    botão de salvar evento na tela

--- lib/features/home/view/tabs/soft_events/soft_events_tabs.dart
+++ lib/features/home/view/tabs/soft_events/soft_events_tabs.dart
@@ -1,4 +1,5 @@
 import 'package:event_keeper/features/home/controller/api_controller.dart';
+import 'package:event_keeper/features/home/controller/prefs_controller.dart';
 import 'package:event_keeper/features/home/view/tabs/soft_events/error/soft_event_load_error_tab.dart';
 import 'package:event_keeper/features/home/view/tabs/soft_events/error/soft_event_non_registered_tab.dart';
 import 'package:event_keeper/shared/components/event_card.dart';
@@ -6,13 +7,15 @@ import 'package:event_keeper/shared/theme/app_color.dart';
 import 'package:event_keeper/shared/core/app_dependencies.dart';
 import 'package:event_keeper/shared/util/app_parses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class SoftEventsTabs extends StatelessWidget {
   SoftEventsTabs({Key? key}) : super(key: key) {
-    controller.getEventList();
+    apiController.getEventList();
   }
 
-  final ApiController controller = getIt<ApiController>();
+  final ApiController apiController = getIt<ApiController>();
+  final PrefsController prefsController = getIt<PrefsController>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +25,13 @@ class SoftEventsTabs extends StatelessWidget {
   Widget _content() {
     return Scaffold(
       body: AnimatedBuilder(
-        animation: controller,
+        animation: apiController,
         builder: (context, _) {
-          return controller.isLoading
+          return apiController.isLoading
               ? const Center(child: CircularProgressIndicator())
-              : controller.onlineEventList.isEmpty
+              : apiController.onlineEventList.isEmpty
                   ? const SoftEventNonRegisteredTab()
-                  : controller.isError
+                  : apiController.isError
                       ? const SoftEventLoadErrorTab()
                       : _listViewBuilder();
         },
@@ -38,47 +41,47 @@ class SoftEventsTabs extends StatelessWidget {
 
   Widget _listViewBuilder() {
     return ListView.builder(
-      itemCount: controller.onlineEventList.length,
+      itemCount: apiController.onlineEventList.length,
       itemBuilder: ((context, index) {
-        final event = controller.onlineEventList[index];
+        final event = apiController.onlineEventList[index];
 
-        return GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, '/details', arguments: event);
-          },
-          child: EventCard(
-            weekDay: AppParses.weekDay(
-              DateTime.parse(event.startTime),
-            ),
-            dayAndMonth: AppParses.dayMonth(
-              DateTime.parse(event.startTime),
-            ),
-            iconButton: IconButton(
-              onPressed: () {},
-              icon:
-                  // const Icon(
-                  //   Icons.check,
-                  //   color: AppColor.green,
-                  //   size: 20,
-                  // ),
-                  const Icon(
-                Icons.close,
-                color: AppColor.red,
-                size: 30,
+        return Observer(builder: (context) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/details', arguments: event);
+            },
+            child: EventCard(
+              weekDay: AppParses.weekDay(
+                DateTime.parse(event.startTime),
               ),
+              dayAndMonth: AppParses.dayMonth(
+                DateTime.parse(event.startTime),
+              ),
+              iconButton: IconButton(
+                onPressed: () {
+                  if (prefsController.savedEventList.contains(event)) {
+                    prefsController.removeFromSavedList(event);
+                  } else {
+                    prefsController.addToSavedList(event);
+                  }
+                },
+                icon: prefsController.savedEventList.contains(event)
+                    ? const Icon(Icons.check, color: AppColor.green, size: 30)
+                    : const Icon(Icons.close, color: AppColor.red, size: 30),
+              ),
+              thumbnail: event.thumbnail,
+              eventName: event.eventName,
+              eventDescription: event.eventDescription,
+              eventStartTime: AppParses.hour(
+                DateTime.parse(event.startTime),
+              ),
+              eventEndTime: AppParses.hour(
+                DateTime.parse(event.endTime),
+              ),
+              eventAddress: event.address?.street ?? '!!ERRO!!',
             ),
-            thumbnail: event.thumbnail,
-            eventName: event.eventName,
-            eventDescription: event.eventDescription,
-            eventStartTime: AppParses.hour(
-              DateTime.parse(event.startTime),
-            ),
-            eventEndTime: AppParses.hour(
-              DateTime.parse(event.endTime),
-            ),
-            eventAddress: event.address?.street ?? '!!ERRO!!',
-          ),
-        );
+          );
+        });
       }),
     );
   }
